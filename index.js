474d265c8a00f1712a09ac0acefae88ce7c1e575
Luke Larsen <luke@lukelarsen.com>
2015-12-07 17:32:17 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,114 @@
+var assign  = require('object-assign');
+var postcss = require('postcss');
+
+var processors = [
+    {
+        plugin:    require('postcss-partial-import'),
+        namespace: 'import',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-each'),
+        namespace: 'each',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-simple-vars'),
+        namespace: 'simpleVars',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-arrow-boxes'),
+        namespace: 'arrowBoxes',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-assemble-modal-helper'),
+        namespace: 'modalHelper',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-assemble-notification-helper'),
+        namespace: 'notificationHelper',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-assemble-table-helper'),
+        namespace: 'tableHelper',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-at2x'),
+        namespace: 'at2x',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-clearfix'),
+        namespace: 'clearfix',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-conditionals'),
+        namespace: 'conditionals',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-custom-media'),
+        namespace: 'media',
+        defaults:  {}
+    },
+    // {
+    //     plugin:    require('postcss-font-magician'),
+    //     namespace: 'fontMagician',
+    //     defaults:  {}
+    // },
+    {
+        plugin:    require('postcss-hidden'),
+        namespace: 'hidden',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-position'),
+        namespace: 'position',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-responsive-type'),
+        namespace: 'responsiveType',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-show-scrollbars'),
+        namespace: 'showScrollbars',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-size'),
+        namespace: 'size',
+        defaults:  {}
+    },
+    {
+        plugin:    require('postcss-zindex-order'),
+        namespace: 'zindexOrder',
+        defaults:  {}
+    }
+];
+
+module.exports = postcss.plugin('assembleCore', function (opts) {
+    opts = assign({}, opts);
+
+    var instance = postcss();
+
+    processors.forEach(function (processor) {
+        var namespaceOptions = processor.namespace in opts ? opts[processor.namespace] : opts;
+        var processorOptions = {};
+
+        processorOptions = assign({}, processor.defaults, namespaceOptions);
+
+        if (namespaceOptions && !processorOptions.disable) {
+            instance.use(processor.plugin(processorOptions));
+        }
+    });
+
+    return instance;
+});
